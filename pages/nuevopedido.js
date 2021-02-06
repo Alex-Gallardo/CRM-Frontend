37868d8592bf81cef5c4a9324eb5e1c092cf105e
Alex Gallardo <gallardoalex069@gmail.com>
2021-02-06 17:42:11 -0600

    validacion de pedido

--- pages/nuevopedido.js
+++ pages/nuevopedido.js
@@ -1,4 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+
+// CONTEXT DE PEDIDOS
+import PedidoContext from '../context/pedidos/PedidoContex'
+
+// APOLLO GQL
+import { gql, useMutation } from '@apollo/client'
+
+// RUTAS NEXT
+import { useRouter } from 'next/router'
+
+// LAYOUT COMPONENT
 import Layout from '../components/Layout'
 
 // ASIGNAR CLIENTE
@@ -10,23 +21,91 @@ import ResumenPedido from '../components/pedidos/ResumenPedido';
 // TOTAL DEL PEDIDO
 import Total from '../components/pedidos/Total';
 
-// CONTEXT DE PEDIDOS
-import PedidoContext from '../context/pedidos/PedidoContex'
+// ALERT
+import Swal from 'sweetalert2'
+
+const NUEVO_PEDIDO = gql`
+    mutation nuevoPedido( $input: PedidoInput ) {
+        nuevoPedido( input: $input ) {
+            id
+        }
+    }
+`
 
 const NuevoPedido = () => {
+
+    // Estate
+    const [mensaje, setMensaje] = useState(null)
 
     // Utilizat contex y extraer sus valores
     const pedidoContext = useContext(PedidoContext)
     const { cliente, productos, total } = pedidoContext
 
+    // Mutation para crear un nuevo pedido
+    const [nuevoPedido] = useMutation(NUEVO_PEDIDO)
+
+    // Rutas
+    const router = useRouter()
+
     const validarPedido = () => {
         // every: itera en todos los productos segun la condicion
-        return !productos.every(producto => producto.cantidad > 0) || total === 0 || cliente.length === 0? ' opacity-50 cursor-not-allowed ' : ''
+        return !productos.every(producto => producto.cantidad > 0) || total === 0 || cliente.length === 0 ? ' opacity-50 cursor-not-allowed ' : ''
+    }
+
+    // Crear un nuevo pedido
+    const crearNuevoPedido = async () => {
+
+        const { id } = cliente
+
+        // Remover lo no deseado de productos
+        const pedido = productos.map(({ __typename, existencias, creado, ...producto }) => producto)
+        console.log('Pedido', pedido)
+
+        try {
+            const { data } = await nuevoPedido({
+                variables: {
+                    input: {
+                        cliente: id,
+                        total,
+                        pedido
+                    }
+                }
+            })
+
+            // Redireccionar 
+            router.push('/pedidos')
+
+            // Mostrar alerta
+            Swal.fire(
+                'Correcto',
+                'El pedido se registro correctamente',
+                'success'
+            )
+
+        } catch (error) {
+            setMensaje(error.message.replace('GraphQL error: ', ''))
+
+            // Borrar el mensaje despues de 3 segundos
+            setTimeout(() => {
+                setMensaje(null)
+            }, 300)
+        }
+    }
+
+    const mostrarMensaje = () => {
+        return (
+            <div className='bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto'>
+                <p>{mensaje}</p>
+            </div>
+        )
     }
 
     return (
         <Layout>
             <h1 className='text-2xl text-gray-800 font-light'>Crear nuevo pedido</h1>
+
+            {mensaje && mostrarMensaje()}
+
             <div className='flex justify-center mt-5'>
                 <div className='w-full max-w-lg'>
                     <AsignarCliente />
@@ -36,6 +115,7 @@ const NuevoPedido = () => {
                     <button
                         type='button'
                         className={`bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 ${validarPedido()}`}
+                        onClick={crearNuevoPedido}
                     >Registrar pedido</button>
                 </div>
             </div>
